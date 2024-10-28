6cc8ae931e805f56d32422089b83dd3a7600d87d
r-serori <155217702+r-serori@users.noreply.github.com>
2024-10-28 12:40:57 +0900

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./styles.css";
+
+import React, { useState } from "react";
+import SearchBar from "./SearchBar";
+import ItemList from "./ItemList";
+
+function App() {
+  return (
+    <div>
+      <h1>検索フィルタアプリ</h1>
+    </div>
+  );
+}
+
+export default App;
